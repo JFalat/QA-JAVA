3157101431a4938803b4973c64be814b81c43bc3
JFalat <joanna.falatowicz@gmail.com>
2017-12-10 11:23:47 +0100

    adding auxiliary methods for Contact

--- addressbook-web-tests/src/test/java/com/qatraining/addressbook/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/com/qatraining/addressbook/ContactCreationTest.java
@@ -18,43 +18,6 @@ import static org.openqa.selenium.OutputType.*;
 public class ContactCreationTest {
     FirefoxDriver wd;
     
-    @BeforeMethod
-    public void setUp() throws Exception {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void ContactCreationTest() {
-        wd.get("http://localhost/addressbook/group.php");
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-        wd.findElement(By.linkText("add new")).click();
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("Joanna");
-        wd.findElement(By.name("middlename")).click();
-        wd.findElement(By.name("middlename")).clear();
-        wd.findElement(By.name("middlename")).sendKeys("test1");
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("theform")).click();
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("joanna@test1.com");
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-        wd.findElement(By.linkText("Logout")).click();
-    }
-    
-    @AfterMethod
-    public void tearDown() {
-        wd.quit();
-    }
-    
     public static boolean isAlertPresent(FirefoxDriver wd) {
         try {
             wd.switchTo().alert();
@@ -62,5 +25,62 @@ public class ContactCreationTest {
         } catch (NoAlertPresentException e) {
             return false;
         }
+    }
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+      wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+      login();
+    }
+
+  private void login() {
+    wd.get("http://localhost/addressbook/group.php");
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+  }
+
+  @Test
+    public void ContactCreationTest() {
+
+    goToContactPage();
+    fillContactForm();
+    submitContact();
+    logout();
+  }
+
+  private void logout() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void submitContact() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void fillContactForm() {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys("Joanna");
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys("test1");
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys("joanna@test1.com");
+  }
+
+  private void goToContactPage() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  @AfterMethod
+    public void tearDown() {
+        wd.quit();
     }
 }
