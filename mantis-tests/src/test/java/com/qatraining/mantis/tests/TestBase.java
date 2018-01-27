42c30f078eede8de469db6e995a9e3a07a35c9fd
JFalat <joanna.falatowicz@gmail.com>
2018-01-27 21:52:38 +0100

    Ex 19

---
+++ mantis-tests/src/test/java/com/qatraining/mantis/tests/TestBase.java
@@ -0,0 +1,25 @@
+package com.qatraining.mantis.tests;
+import com.qatraining.mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+
+public class TestBase {
+
+
+  public static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() {
+    app.stop();
+  }
+
+}
